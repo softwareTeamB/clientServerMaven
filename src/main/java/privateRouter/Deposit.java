9bc8bcd616f43b2739ec030488cb379a1b1eab91
michel <micheldaniel2@hotmail.com>
2017-11-13 15:43:40 +0100

    Kleine deposit verbetering

--- src/main/java/privateRouter/Deposit.java
+++ src/main/java/privateRouter/Deposit.java
@@ -7,6 +7,7 @@ import global.ErrorMessageResponse;
 import mysql.Mysql;
 import privateRouter.packageApi.BittrexProtocall;
 import Mail.MailServer;
+import javax.mail.MessagingException;
 
 /**
  * Klasse die kijkt of er nieuwe deposit
@@ -19,9 +20,10 @@ public class Deposit {
     BittrexProtocall bp = new BittrexProtocall();
     Mysql mysql = new Mysql();
     MailServer mail = new MailServer();
-    
+
     //change is er geweest
     private boolean addTransactie = false;
+    private String htmlBerichtString = "";
 
     /**
      * Main methoden
@@ -86,7 +88,16 @@ public class Deposit {
             } catch (Exception ex) {
                 ConsoleColor.err("Er is een error op getreden bij depositCheck. Dit is de error: " + ex);
             }
+        }
 
+        //kijk of de addTransactie veranderd is
+        if (addTransactie) {
+            try {
+                mail.generateAndSendEmail("", htmlBerichtString);
+                ConsoleColor.out("Er is een mail bericht verstuurd.");
+            } catch (MessagingException ex) {
+                ConsoleColor.err("Er is een probleem bij het sturen van een mail. "+ex);
+            }
         }
     }
 
@@ -101,13 +112,14 @@ public class Deposit {
     private void depositCheck(String txId, int idExchange, double amount, String cointag) throws Exception {
 
         //sqlcount string
-        String sqlCount = "SELECT COUNT(*) AS total FROM depositLijst WHERE txid='" + txId + 
-                "' AND idExchangeLijst='" + idExchange + "'";
+        String sqlCount = "SELECT COUNT(*) AS total FROM depositLijst WHERE txid='" + txId
+                + "' AND idExchangeLijst='" + idExchange + "'";
         int count = mysql.mysqlCount(sqlCount);
         if (count == 0) {
-            
+
             //zet de variable dat er een vandereing is op true
-            
+            addTransactie = true;
+
             //voeg de transactie in de database
             String sqlInsert = "INSERT INTO depositLijst (txid, idExchangeLijst, cointag, hoeveelheid) "
                     + "VALUES ('" + txId + "', '" + idExchange + "', '" + cointag + "', '" + amount + "')";
@@ -119,8 +131,8 @@ public class Deposit {
             String htmlBody = "<p> txId: " + txId + "</p><p>exchange naam: " + idExchange + "</p>"
                     + "<p> Cointag: " + cointag + "</p><p>Hoeveelheid: " + amount + "</p><br>";
 
-            mail.generateAndSendEmail("", htmlBody);
-
+            //voeg de html string toe in de andere html string
+            htmlBerichtString += htmlBody;
         }
 
     }
