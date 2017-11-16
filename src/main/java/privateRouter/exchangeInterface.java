21c0eb4e65e45ec58fdf9ad251e314950cf0b439
michel <micheldaniel2@hotmail.com>
2017-11-16 11:22:57 +0100

    update interface klasse

---
+++ src/main/java/privateRouter/exchangeInterface.java
@@ -0,0 +1,83 @@
+package privateRouter;
+
+/**
+ * Hier in staat belangrijke methoden die de communicatie moeten regelen met de exchange
+ *
+ * @author michel
+ */
+public interface exchangeInterface {
+
+    /**
+     * Order status
+     *
+     * @return string met de order status
+     * @throws java.lang.Exception error afvang systeem
+     */
+    public String orderStatus() throws Exception;
+
+    /**
+     * Cancel order
+     *
+     * @return of het succes vol de order is gecancelt
+     * @throws java.lang.Exception error afvang systeem
+     */
+    public boolean cancelOrder() throws Exception;
+
+    /**
+     * Plaats order
+     *
+     * @return UUID komt terug als het goed gaat
+     * @throws java.lang.Exception error afvang systeem
+     */
+    public String setOrder(String baseCoin, String marktCoin, double prijs, double hoebeelheid, String type) throws Exception;
+
+    /**
+     * Vraag de orders op die uitstaan bij de exchange
+     *
+     * @return
+     * @throws java.lang.Exception error afvang systeem
+     */
+    public String getOrders() throws Exception;
+
+    /**
+     * Vraag de orders op die uitstaan bij de exchange
+     *
+     * @param uuid order id
+     * @return de order status
+     * @throws java.lang.Exception error afvang systeem
+     */
+    public String getOrder(String uuid) throws Exception;
+
+    /**
+     * Cancel order
+     *
+     * @param uuid order id
+     * @return order succusvol is
+     */
+    public boolean cancelOrder(String uuid);
+
+    /**
+     * Krijg de balance. Positie [0] is balance. Positie [1] available. Positie [2] pending balance
+     *
+     * @param currency cointag
+     * @return return string array
+     * @throws java.lang.Exception error afvang
+     */
+    public double[] getBalance(String currency) throws Exception;
+
+    /**
+     * Haal de balance op van de server
+     *
+     * @throws Exception Error
+     */
+    public void setServerBalance() throws Exception;
+
+    /**
+     *
+     * @param balanceArray String array met de balance waarde.Positie[0] balance Positie [1] available. Positie [2]
+     * pending balance
+     * @param currency cointag
+     * @throws Exception error exception
+     */
+    public void setBalance(double[] balanceArray, String currency) throws Exception;
+}
